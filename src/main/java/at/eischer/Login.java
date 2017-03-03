37803c10a6fefe7adb8dc9afd34142e66fb9d190
Eischer <spenger17>
2017-03-03 22:57:27 +0100

    Starting the implementation of the persiting (not working status)

--- src/main/java/at/eischer/Login.java
+++ src/main/java/at/eischer/Login.java
@@ -1,12 +1,19 @@
 package at.eischer;
 
+import at.eischer.model.Team;
+
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.io.Serializable;
 
 @Named
 @SessionScoped
 public class Login implements Serializable{
+
+    @PersistenceContext(unitName = "myPersistenceUnit")
+    private EntityManager entityManager;
 
     private String name;
 
@@ -19,6 +26,9 @@ public class Login implements Serializable{
     }
 
     public String send() {
+        Team team = new Team();
+        team.setName("Grasshoppers West Wien");
+        entityManager.persist(team);
         return ("index");
     }
 }
