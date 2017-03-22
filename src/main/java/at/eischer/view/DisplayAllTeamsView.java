89e10e2d461482fc957fe7700495fd7d34eccbf7
Bernhard Eischer <bernhard.eischer@itsv.at>
2017-03-22 17:24:12 +0100

    Use of Primeface for displaying logos now working

--- src/main/java/at/eischer/view/DisplayAllTeamsView.java
+++ src/main/java/at/eischer/view/DisplayAllTeamsView.java
@@ -2,27 +2,36 @@ package at.eischer.view;
 
 import at.eischer.model.Team;
 import at.eischer.services.TeamService;
+import org.primefaces.model.DefaultStreamedContent;
+import org.primefaces.model.StreamedContent;
 
 import javax.annotation.PostConstruct;
-import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.context.RequestScoped;
+import javax.faces.context.FacesContext;
+import javax.faces.event.PhaseId;
 import javax.inject.Inject;
 import javax.inject.Named;
+import java.io.ByteArrayInputStream;
 import java.util.List;
 
 @Named
-@ApplicationScoped
+@RequestScoped
 public class DisplayAllTeamsView {
 
     private List<Team> allTeams;
 
+    public TeamService getTeamService() {
+        return teamService;
+    }
+
+    public void setTeamService(TeamService teamService) {
+        this.teamService = teamService;
+    }
+
+    private StreamedContent logo;
+
     @Inject
     TeamService teamService;
-
-    public byte[] getLogo(int teamId) {
-        Team team = teamService.findTeamById(teamId);
-        return team.getLogo();
-    }
 
     @PostConstruct
     public void init() {
@@ -40,6 +49,29 @@ public class DisplayAllTeamsView {
 
     public void setAllTeams(List<Team> allTeams) {
         this.allTeams = allTeams;
+    }
+
+    public StreamedContent getLogo() {
+        FacesContext context = FacesContext.getCurrentInstance();
+
+        if (context.getCurrentPhaseId() == PhaseId.RENDER_RESPONSE) {
+            return new DefaultStreamedContent();
+        }
+
+        else {
+            String id = context.getExternalContext().getRequestParameterMap()
+                    .get("teamId");
+            Team team = teamService.findTeamById(Integer.parseInt(id));
+
+            byte[] image = team.getLogo();
+
+            return new DefaultStreamedContent(new ByteArrayInputStream(image));
+
+        }
+    }
+
+    public void setLogo(StreamedContent logo) {
+        this.logo = logo;
     }
 
 }
