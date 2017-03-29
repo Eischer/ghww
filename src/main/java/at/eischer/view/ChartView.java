24942e55108a39ab4cf7ae95b6708fe651f08f0c
Bernhard Eischer <bernhard.eischer@itsv.at>
2017-03-29 18:03:37 +0200

    Hello World Line Chart

---
+++ src/main/java/at/eischer/view/ChartView.java
@@ -0,0 +1,67 @@
+package at.eischer.view;
+
+import org.primefaces.model.chart.Axis;
+import org.primefaces.model.chart.AxisType;
+import org.primefaces.model.chart.LineChartModel;
+import org.primefaces.model.chart.LineChartSeries;
+
+import javax.annotation.PostConstruct;
+import javax.inject.Named;
+
+@Named
+public class ChartView {
+
+    private LineChartModel lineModel1;
+
+    @PostConstruct
+    public void init() {
+        createLineModels();
+    }
+
+    private void createLineModels() {
+        lineModel1 = initLinearModel();
+        lineModel1.setTitle("Linear Chart");
+        lineModel1.setLegendPosition("e");
+        Axis yAxis = lineModel1.getAxis(AxisType.Y);
+        yAxis.setMin(0);
+        yAxis.setMax(10);
+    }
+
+    private LineChartModel initLinearModel() {
+        LineChartModel model = new LineChartModel();
+
+        LineChartSeries series1 = new LineChartSeries();
+        series1.setLabel("Series 1");
+
+        series1.set(1, 2);
+        series1.set(2, 1);
+        series1.set(3, 3);
+        series1.set(4, 6);
+        series1.set(5, 8);
+
+        LineChartSeries series2 = new LineChartSeries();
+        series2.setLabel("Series 2");
+
+        series2.set(1, 6);
+        series2.set(2, 3);
+        series2.set(3, 2);
+        series2.set(4, 7);
+        series2.set(5, 9);
+
+        model.addSeries(series1);
+        model.addSeries(series2);
+
+        return model;
+    }
+
+    /**
+     * GETTER and SETTERS
+     */
+    public LineChartModel getLineModel1() {
+        return lineModel1;
+    }
+
+    public void setLineModel1(LineChartModel lineModel1) {
+        this.lineModel1 = lineModel1;
+    }
+}
