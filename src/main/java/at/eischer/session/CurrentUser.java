e10c86272c26e53b30256e899e2220439206d23a
Bernhard Eischer <bernhard.eischer@itsv.at>
2017-04-06 17:19:22 +0200

    Logout implemented

--- src/main/java/at/eischer/session/CurrentUser.java
+++ src/main/java/at/eischer/session/CurrentUser.java
@@ -3,12 +3,20 @@ package at.eischer.session;
 import at.eischer.model.User;
 
 import javax.enterprise.context.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
 import java.io.Serializable;
 
+@Named
 @SessionScoped
 public class CurrentUser implements Serializable{
 
     private User user;
+
+    public String logout() {
+        FacesContext.getCurrentInstance().getExternalContext().invalidateSession();
+        return "/public/login.xhtml";
+    }
 
     public User getUser() {
         return user;
